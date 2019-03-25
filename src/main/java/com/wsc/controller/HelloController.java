a9a7690ee8ce3b1252484b4a3ead789ada149987
lamda2019 <569188901@qq.com>
2019-03-25 20:44:05 +0800

    根据请求源的不同，返回错误方式不同，页面请求返回错误页面，json请求返回错误json

--- src/main/java/com/wsc/controller/HelloController.java
+++ src/main/java/com/wsc/controller/HelloController.java
@@ -1,7 +1,9 @@
 package com.wsc.controller;
 
+import com.wsc.exception.UserNotExistException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Arrays;
@@ -11,16 +13,26 @@ import java.util.Map;
 @Controller
 public class HelloController {
 
+        /*
+            这里是测试自定义的错误页面和错误json
+            页面返回错误页面
+            postman返回错误json
+            */
+
     @ResponseBody
     @RequestMapping("/hello")
-    public  String hello(){
+
+    public String hello(@RequestParam("user") String user) {
+        if (user.equals("aaa")) {
+            throw new UserNotExistException();
+        }
         return "Hello World";
     }
 
     @RequestMapping("/success")
-    public String success(Map<String,Object> map){
-        map.put("hello","<h1>你好</h1>");
-        map.put("users", Arrays.asList("紫金山","头驼铃","山顶公园"));
+    public String success(Map<String, Object> map) {
+        map.put("hello", "<h1>你好</h1>");
+        map.put("users", Arrays.asList("紫金山", "头驼铃", "山顶公园"));
         return "success";
     }
 }
