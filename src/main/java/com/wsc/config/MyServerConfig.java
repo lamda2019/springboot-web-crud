5a9876aae2dcceea6fff3bae3a3a05c74573f83c
lamda2019 <569188901@qq.com>
2019-03-25 21:34:26 +0800

    注册三大组件

--- src/main/java/com/wsc/config/MyServerConfig.java
+++ src/main/java/com/wsc/config/MyServerConfig.java
@@ -1,9 +1,17 @@
 package com.wsc.config;
 
+import com.wsc.filter.MyFilter;
+import com.wsc.listener.MyListener;
+import com.wsc.servlet.MyServlet;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @Configuration
 public class MyServerConfig {
@@ -21,5 +29,43 @@ public class MyServerConfig {
         };
     }
 
+
+
+    //注册三大组件
+
+    /*
+     * 通过http://localhost:8083/myServlet
+     * 访问
+     * */
+    @Bean
+    public ServletRegistrationBean myServlet(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new MyServlet(),"/myServlet");
+        registrationBean.setLoadOnStartup(1);
+        return registrationBean;
+    }
+
+    /*拦截http://localhost:8083/hello，http://localhost:8083/myServlet
+     * 控制台输出：
+     *“ MyFilter process...”
+     *
+     * */
+    @Bean
+    public FilterRegistrationBean myFilter(){
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new MyFilter());
+        registrationBean.setUrlPatterns(Arrays.asList("/hello","/myServlet"));
+        return registrationBean;
+    }
+    /*
+    * 控制台可看到
+    * “contextInitialized...web应用启动”
+    * “contextDestroyed...当前web项目销毁”
+    *
+    * */
+    @Bean
+    public ServletListenerRegistrationBean myListener(){
+        ServletListenerRegistrationBean<MyListener> registrationBean = new ServletListenerRegistrationBean<>(new MyListener());
+        return registrationBean;
+    }
 
 }
