f4234af9be333c8bae3311a1b676f0bff5a0e4e1
lamda2019 <569188901@qq.com>
2019-03-25 21:03:05 +0800

    配置嵌入式的Servlet容器

---
+++ src/main/java/com/wsc/config/MyServerConfig.java
@@ -0,0 +1,25 @@
+package com.wsc.config;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyServerConfig {
+
+    //配置嵌入式的Servlet容器
+    @Bean
+    public EmbeddedServletContainerCustomizer embeddedServletContainerCustomizer(){
+        return new EmbeddedServletContainerCustomizer() {
+
+            //定制嵌入式的Servlet容器相关的规则
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+                container.setPort(8083);
+            }
+        };
+    }
+
+
+}
